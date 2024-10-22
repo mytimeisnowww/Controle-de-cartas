56407734414c89ef2a5074c73a03ff91244e863a
Rosim <davi.rosim@ges.inatel.br>
2024-10-22 20:04:16 -0300

    Update CartaService.java

--- src/main/java/CartaService.java
+++ src/main/java/CartaService.java
@@ -9,17 +9,6 @@ public class CartaService {
     @Autowired
     private CartaRepository cartaRepository;
 
-    public Limport org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import java.util.List;
-import java.util.Optional;
-
-@Service
-public class CartaService {
-
-    @Autowired
-    private CartaRepository cartaRepository;
-
     public List<Carta> listarCartas() {
         return cartaRepository.findAll();
     }
